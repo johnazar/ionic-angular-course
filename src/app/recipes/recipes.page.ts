b83e3ac28242914f048a0483d106b02892d491a5
John Azar <eng.johnazar@gmail.com>
2020-05-23 00:42:14 +0300

    recipes added

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
+  recipes: Recipe[] = [{
+    id:'r1',
+    title:'Title 1',
+    imageUrl:'https://i.picsum.photos/id/305/300/300.jpg',
+    ingredients:['material1','material2']
+  },
+  {
+    id:'r2',
+    title:'Title 2',
+    imageUrl:'https://i.picsum.photos/id/301/300/300.jpg',
+    ingredients:['material1','material2']
+  }];
   constructor() { }
 
   ngOnInit() {
